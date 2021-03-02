a1ce2dc7b98eae94c70fcdee1f393a8d129f2ef3
Rodrigo Pombo <pombopombopombo@gmail.com>
2021-03-02 15:12:25 -0300

    Add scrollycoding

--- components/mdx-components.js
+++ components/mdx-components.js
@@ -1,7 +1,30 @@
 import CodeBlock from './code-block'
 import CustomLink from './custom-link'
+import {
+  Hike as HikeSteps,
+  Focus,
+  CodeSlot,
+  PreviewSlot,
+  useMdxSteps,
+  StepHead,
+  withFocusHandler,
+} from '@code-hike/scrollycoding'
+
+function Hike({ children, previewProps, codeProps, ...props }) {
+  const steps = useMdxSteps(children, previewProps, {
+    ...codeProps,
+    minColumns: 40,
+  })
+  return <HikeSteps steps={steps} {...props} />
+}
 
 export const components = {
-  a: CustomLink,
   pre: CodeBlock,
+  // Code Hike:
+  a: withFocusHandler(CustomLink),
+  Hike,
+  Focus,
+  StepHead,
+  CodeSlot,
+  PreviewSlot,
 }
